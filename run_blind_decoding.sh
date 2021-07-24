45adf1a919fe092c57c7543c77891f9b6947c7f4
Mari Ganesh Kumar M <mariganeshkumar@gmail.com>
2021-07-24 12:50:43 +0530

    version 1 decoding added

--- run_blind_decoding.sh
+++ run_blind_decoding.sh
@@ -7,15 +7,15 @@
 
 # general configuration
 backend=pytorch
-stage=1      
-stop_stage=1
+stage=2      
+stop_stage=2
 ngpu=2         # number of gpus ("0" uses cpu, otherwise use gpu)
 nj=60
 debugmode=1
 dumpdir=dump   # directory to dump full features
 N=0            # number of minibatches to be used (mainly for debugging). "0" uses all minibatches.
 verbose=0      # verbose option
-resume=exp/train_multi_lingal_pytorch_train_specaug/results/snapshot.ep.18        # Resume the training from snapshot
+resume=        # Resume the training from snapshot
 
 # feature configuration
 do_delta=false
@@ -42,15 +42,8 @@ lm_n_average=0               # the number of languge models to be averaged
 use_lm_valbest_average=false # if true, the validation `lm_n_average`-best language models will be averaged.
                              # if false, the last `lm_n_average` language models will be averaged.
 
-# Set this to somewhere where you want to put your data, or where
-# someone else has already put it.  You'll want to change this
-# if you're not on the CLSP grid.
-datadir=/speech/datasets/IndicASRChallenge
-ai4bharathdir=/speech/datasets/ai4bharat/data
-musan_root=/speech/datasets/musan
 
-# base url for downloads.
-data_url=www.openslr.org/resources/12
+
 
 # bpemode (unigram or bpe)
 nbpe=5000
@@ -68,18 +61,16 @@ set -e
 set -u
 set -o pipefail
 
-train_set=subtask1
-train_sp=train_sp
-train_dev=dev
-recog_set="Tamil-dev Telugu-dev Gujarati-dev Hindi-dev Marathi-dev Odia-dev"
+
 
 dumpdir=dump/
 
 . ./path.sh || exit 1;
 . ./cmd.sh || exit 1;
 
+train_set=subtask1
 expdir=exp/subtask1
-dict=${expdir}/${train_set}_${bpemode}${nbpe}_units.txt
+dict=${expdir}/${train_set}_units.txt
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     echo "stage 1: Decoding Blind Data"
     if [ ! -d "subtask1_blindtest/audio/" ] 
@@ -92,21 +83,21 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     fi
     
     python local/blind_data_prep.py --source \
-            subtask1_blindtest/audio/  --destination data/blind-test
+            subtask1_blindtest/audio/  --destination data/blind-test-task1
 
     fbankdir=${dumpdir}/fbank         
-    local/fix_data_dir.sh data/blind-test
+    local/fix_data_dir.sh data/blind-test-task1
     steps/make_fbank_pitch.sh --cmd "$train_cmd" --nj ${nj} --write_utt2num_frames true \
-       data/blind-test exp/make_fbank/blind-test ${fbankdir}
-    local/fix_data_dir.sh data/blind-test 
+       data/blind-test-task1 exp/make_fbank/blind-test-task1 ${fbankdir}
+    local/fix_data_dir.sh data/blind-test-task1 
 
-    feat_recog_dir=${dumpdir}/blind-test/delta${do_delta}; mkdir -p ${feat_recog_dir}
+    feat_recog_dir=${dumpdir}/blind-test-task1/delta${do_delta}; mkdir -p ${feat_recog_dir}
     dump.sh --cmd "$train_cmd" --nj ${nj} --do_delta ${do_delta} \
-        data/blind-test/feats.scp ${expdir}/cmvn.ark exp/dump_feats/recog/blind-test \
+        data/blind-test-task1/feats.scp ${expdir}/cmvn.ark exp/dump_feats/recog/blind-test-task1 \
         ${feat_recog_dir}
 
     data2json.sh --feat ${feat_recog_dir}/feats.scp --trans_type char \
-        data/blind-test ${dict} > ${feat_recog_dir}/data.json 
+        data/blind-test-task1 ${dict} > ${feat_recog_dir}/data.json 
 
     
     # split data
@@ -115,15 +106,12 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     #### use CPU for decoding
     ngpu=0
 
-    if ${use_valbest_average}; then
-        recog_model=model.val${n_average}.avg.best
-        opt="--log ${expdir}/results/log"
-    else
-        recog_model=model.last${n_average}.avg.best
-        opt="--log"
-    fi
+    
+    recog_model=model.last${n_average}.avg.best
+    opt="--log"
 
-    decode_dir=decode_blind-test_${recog_model}_$(basename ${decode_config%.*})_${lmtag}
+
+    decode_dir=decode_blind-test-task1_${recog_model}_$(basename ${decode_config%.*})_${lmtag}
     
 
     lang_model=rnnlm.model.best
@@ -143,8 +131,7 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
         --rnnlm ${expdir}/${lang_model} \
         --rnnlm-conf ${expdir}/rnnlm-model.json
 
-    d_dir=${expdir}/decode_blind-test_${recog_model}_$(basename ${decode_config%.*})_${lmtag}
-
+    d_dir=${expdir}/${decode_dir}
 
     concatjson.py ${d_dir}/data.*.json > ${d_dir}/data.json
 
@@ -157,82 +144,90 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
 
     sed 's/-.*//' ${d_dir}/hyp.wrd.trn > ${d_dir}/submission-file.s1
     sed 's/(/\t/' ${d_dir}/submission-file.s1 > ${d_dir}/submission-file.s2
-    awk 'BEGIN {FS="\t"; OFS=" "} {print $2, $1}' ${d_dir}/submission-file.s2 > ${d_dir}/submission-file
+    awk 'BEGIN {FS="\t"; OFS=" "} {print $2, $1}' ${d_dir}/submission-file.s2 > subtask1_submission_file.txt
 
 fi
 
-dict=data/lang_1char/${train_set}_units.txt
-cls_dict=data/lang_1char/${train_set}_cls_units.txt
-
+train_set=subtask2
+expdir=exp/subtask2
+dict=${expdir}/${train_set}_${bpemode}${nbpe}_units.txt
+bpemodel=${expdir}/${train_set}_${bpemode}${nbpe}
+task2_blind_set="Bengali-English Hindi-English"
 if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
-    echo "stage 2: Decoding Blind Data using model 2"
-    nj=60
-    python local/blind_data_prep.py --source \
-            /speech/datasets/IndicASRChallenge/blind_test1/audio/  --destination data/blind-test
-
-    fbankdir=${dumpdir}/fbank         
-    local/fix_data_dir.sh data/blind-test
-    steps/make_fbank_pitch.sh --cmd "$train_cmd" --nj ${nj} --write_utt2num_frames true \
-       data/blind-test exp/make_fbank/blind-test ${fbankdir}
-    local/fix_data_dir.sh data/blind-test 
-
-    feat_recog_dir=${dumpdir}/blind-test/delta${do_delta}; mkdir -p ${feat_recog_dir}
-    dump.sh --cmd "$train_cmd" --nj ${nj} --do_delta ${do_delta} \
-        data/blind-test/feats.scp data/${train_sp}/cmvn.ark exp/dump_feats/recog/blind-test \
-        ${feat_recog_dir}
-
-    data2json.sh --feat ${feat_recog_dir}/feats.scp --trans_type char \
-        data/blind-test ${dict} > ${feat_recog_dir}/data.json 
-
-    
-    # split data
-    splitjson.py --parts ${nj} ${feat_recog_dir}/data.json
-
-    #### use CPU for decoding
-    ngpu=0
-
-    if ${use_valbest_average}; then
-        recog_model=model.val${n_average}.avg.best
-        opt="--log ${expdir}/results/log"
-    else
-        recog_model=model.last${n_average}.avg.best
-        opt="--log"
+    echo "stage 2: Decoding Blind Data"
+    if [ ! -d "subtask2_blindtest/" ] 
+    then
+        rm -rf subtask2_blindtest/
+        echo "Directory subtask2_blindtest/audio/ DOES NOT exists downloading data."
+        wget http://www.ee.iisc.ac.in/people/faculty/prasantg/downloads/subtask2_blindtest.tar.gz
+        mkdir -p subtask2_blindtest
+        tar -xf subtask2_blindtest.tar.gz -C subtask2_blindtest
     fi
 
-    decode_dir=decode_blind-test_${recog_model}_$(basename ${decode_config%.*})_${lmtag}
+    rm -rf submission-file-task2.txt
+
+    for rtask in ${task2_blind_set}; do
     
+        python local/blind_data_prep_task2.py --source \
+                /speech/datasets/IndicASRChallenge/blind_test2/${rtask}/  --destination data/blind-test-task2-${rtask}
+        fbankdir=${dumpdir}/fbank         
+        local/fix_data_dir.sh data/blind-test-task2-${rtask}
 
-    lang_model=rnnlm.model.best
+        steps/make_fbank_pitch.sh --cmd "$train_cmd" --nj ${nj} \
+        --write_utt2num_frames true data/blind-test-task2-${rtask} \
+        exp/make_fbank/blind-test-task2-${rtask} ${fbankdir}
+        local/fix_data_dir.sh data/blind-test-task2-${rtask}
 
-    # set batchsize 0 to disable batch decoding
-    ${decode_cmd} JOB=1:${nj} ${expdir}/${decode_dir}/log/decode.JOB.log \
-        asr_dual_recog.py \
-        --config ${decode_config} \
-        --ngpu ${ngpu} \
-        --backend ${backend} \
-        --batchsize 0 \
-        --recog-json ${feat_recog_dir}/split${nj}utt/data.JOB.json \
-        --result-label ${expdir}/${decode_dir}/data.JOB.json \
-        --model ${expdir}/${recog_model}  \
-        --model-conf ${expdir}/model.json \
-        --api v2 \
-        --rnnlm ${lmexpdir}/${lang_model} \
-        --rnnlm-conf ${expdir}/rnnlm-model.json
+        feat_recog_dir=${dumpdir}/blind-test-task2-${rtask}/delta${do_delta}
+        mkdir -p ${feat_recog_dir}
+        
+        dump.sh --cmd "$train_cmd" --nj ${nj} --do_delta ${do_delta} \
+        data/blind-test-task2-${rtask}/feats.scp \
+        ${expdir}/cmvn.ark exp/dump_feats/recog/blind-test-task2-${rtask} \
+        ${feat_recog_dir}
+ 
+        data2json.sh --feat ${feat_recog_dir}/feats.scp --trans_type char \
+        data/blind-test-task2-${rtask} ${dict} > ${feat_recog_dir}/data.json 
 
-    d_dir=${expdir}/decode_blind-test_${recog_model}_$(basename ${decode_config%.*})_${lmtag}
+        # split data
+        splitjson.py --parts ${nj} ${feat_recog_dir}/data.json
 
+        #### use CPU for decoding
+        ngpu=0
 
-    concatjson.py ${d_dir}/data.*.json > ${d_dir}/data.json
+        recog_model=model.val${n_average}.avg.best
+        opt="--log ${expdir}/results/log"
 
-    json2trn.py ${d_dir}/data.json ${dict} --num-spkrs 1 --refs ${d_dir}/ref.trn --hyps ${d_dir}/hyp.trn
+        decode_dir=decode_blind-test-task2-${rtask}_${recog_model}_$(basename ${decode_config%.*})_${lmtag}
+        
 
-    sed -i.bak2 -r 's/<blank> //g' ${d_dir}/hyp.trn
+        # set batchsize 0 to disable batch decoding
+        ${decode_cmd} JOB=1:${nj} ${expdir}/${decode_dir}/log/decode.JOB.log \
+            asr_dual_recog.py \
+            --config ${decode_config} \
+            --ngpu ${ngpu} \
+            --backend ${backend} \
+            --batchsize 0 \
+            --recog-json ${feat_recog_dir}/split${nj}utt/data.JOB.json \
+            --result-label ${expdir}/${decode_dir}/data.JOB.json \
+            --model ${expdir}/${recog_model}  \
+            --api v2 
 
-    sed -e "s/ //g" -e "s/(/ (/" -e "s/<space>/ /g" ${d_dir}/ref.trn > ${d_dir}/ref.wrd.trn
-    sed -e "s/ //g" -e "s/(/ (/" -e "s/<space>/ /g" ${d_dir}/hyp.trn > ${d_dir}/hyp.wrd.trn
+        d_dir=${expdir}/${decode_dir}
 
-    sed 's/-.*//' ${d_dir}/hyp.wrd.trn > ${d_dir}/submission-file.s1
-    sed 's/(/\t/' ${d_dir}/submission-file.s1 > ${d_dir}/submission-file.s2
-    awk 'BEGIN {FS="\t"; OFS=" "} {print $2, $1}' ${d_dir}/submission-file.s2 > ${d_dir}/submission-file
+        concatjson.py ${d_dir}/data.*.json > ${d_dir}/data.json
 
+        json2trn.py ${d_dir}/data.json ${dict} --num-spkrs 1 --refs ${d_dir}/ref.trn --hyps ${d_dir}/hyp.trn
+
+        sed -i.bak2 -r 's/<blank> //g' ${d_dir}/hyp.trn
+
+        sed -e "s/ //g" -e "s/(/ (/" -e "s/<space>/ /g" ${d_dir}/ref.trn > ${d_dir}/ref.wrd.trn
+        sed -e "s/ //g" -e "s/(/ (/" -e "s/<space>/ /g" ${d_dir}/hyp.trn > ${d_dir}/hyp.wrd.trn
+
+        sed 's/-.*//' ${d_dir}/hyp.wrd.trn > ${d_dir}/submission-file.s1
+        sed 's/(/\t/' ${d_dir}/submission-file.s1 > ${d_dir}/submission-file.s2
+        awk 'BEGIN {FS="\t"; OFS=" "} {print $2, $1}' ${d_dir}/submission-file.s2 > submission-file-task2-${rtask}.txt
+        cat submission-file-task2-${rtask}.txt >> submission-file-task2.txt
+    done            
 fi
+
